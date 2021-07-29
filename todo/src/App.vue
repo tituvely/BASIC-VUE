d598d8c7e25ee8f08e0fa7508b4322acc991173b
Titu YooKyung Kim <tituvely@gmail.com>
2021-07-29 12:10:51 +0900

    todo app

---
+++ todo/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app" class="container">
+    <h1 class="text-center">TODO APP</h1>
+    <input
+      v-model="todoText"
+      type="text"
+      class="w-100 p-2"
+      placeholder="Type todo"
+      @keyup.enter="addTodo"
+    />
+    <hr />
+    <Todo
+      v-for="todo in todos"
+      :key="todo.id"
+      :todo="todo"
+      @toggle-checkbox="toggleCheckbox"
+      @click-delete="deleteTodo"
+    />
+  </div>
+</template>
+
+<script>
+import Todo from "@/components/Todo.vue";
+
+export default {
+  components: {
+    Todo,
+  },
+  data() {
+    return {
+      todoText: "",
+      todos: [
+        { id: 1, text: "buy a car", checked: false },
+        { id: 2, text: "play game", checked: true },
+      ],
+    };
+  },
+  methods: {
+    addTodo(e) {
+      this.todos.push({
+        id: Math.random(),
+        text: e.target.value,
+        checked: false,
+      });
+      this.todoText = "";
+    },
+    toggleCheckbox({ id, checked }) {
+      const index = this.todos.findIndex((todo) => {
+        return todo.id === id;
+      });
+      this.todos[index].checked = checked;
+    },
+    deleteTodo(id) {
+      // const index = this.todos.findIndex((todo) => {
+      //   return todo.id === id;
+      // });
+      // this.todos.splice(index, 1);
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+  },
+};
+</script>
+
